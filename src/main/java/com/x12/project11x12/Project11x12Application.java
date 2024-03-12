3161fbe355cb707d9b3fbc4573f07347d792c44f
Borja <elbarmandev@gmail.com>
2024-03-12 15:15:43 +0100

    first commit

---
+++ src/main/java/com/x12/project11x12/Project11x12Application.java
@@ -0,0 +1,13 @@
+package com.x12.project11x12;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Project11x12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Project11x12Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/x12/project11x12/Project11x12ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x12.project11x12;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Project11x12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
