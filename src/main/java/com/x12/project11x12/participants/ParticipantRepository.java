6b21b08a9bacec16adc3f225df52629f62d2685c
Luis Pardo <luispardosuarez@gmail.com>
2024-04-07 13:03:32 +0200

    Implemented ParticipantRepository without delete operation

--- src/main/java/com/x12/project11x12/participants/ParticipantRepository.java
+++ src/main/java/com/x12/project11x12/participants/ParticipantRepository.java
@@ -1,5 +1,8 @@
-package com.x12.project11x12.participant;
+package com.x12.project11x12.participants;
 
-public class ParticipantRepository {
-  
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ParticipantRepository extends JpaRepository<Participant, Long> {
 }
