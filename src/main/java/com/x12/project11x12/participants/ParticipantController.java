691f5e22de4d0b13f819a8170b6be153b0e90b3e
Luis Pardo <luispardosuarez@gmail.com>
2024-04-07 12:56:25 +0200

    Added ParticipantController for handling CRUD operations (excluding deletion) on Participant entities

--- src/main/java/com/x12/project11x12/participants/ParticipantController.java
+++ src/main/java/com/x12/project11x12/participants/ParticipantController.java
@@ -1,5 +1,44 @@
-package com.x12.project11x12.participant;
+package com.x12.project11x12.participants;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/participants")
 public class ParticipantController {
-  
+
+    private final ParticipantService participantService;
+
+    @Autowired
+    public ParticipantController(ParticipantService participantService) {
+        this.participantService = participantService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Participant>> getAllParticipants() {
+        List<Participant> participants = participantService.getAllParticipants();
+        return new ResponseEntity<>(participants, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Participant> getParticipantById(@PathVariable Long id) {
+        Participant participant = participantService.getParticipantById(id);
+        return new ResponseEntity<>(participant, HttpStatus.OK);
+    }
+
+    @PostMapping
+    public ResponseEntity<Participant> createParticipant(@RequestBody Participant participant) {
+        Participant createdParticipant = participantService.createParticipant(participant);
+        return new ResponseEntity<>(createdParticipant, HttpStatus.CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Participant> updateParticipant(@PathVariable Long id, @RequestBody Participant participantDetails) {
+        Participant updatedParticipant = participantService.updateParticipant(id, participantDetails);
+        return new ResponseEntity<>(updatedParticipant, HttpStatus.OK);
+    }
 }
