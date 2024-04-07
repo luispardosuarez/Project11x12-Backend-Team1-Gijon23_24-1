d9d2402a18b37a29a7072087cda720f59105e7ed
Luis Pardo <luispardosuarez@gmail.com>
2024-04-07 13:18:24 +0200

    Added data for Participant entity in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,3 +11,7 @@ INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '10000000X');
 
 -- Profile
 INSERT INTO profiles (id, dni, profile_name, profile_surname, tlf1, tlf2) VALUES (1, '12345678A', 'Juan', 'Perez', '912345678', '987654321');
+
+-- Participant
+INSERT INTO participants (id_participant, participant_name, participant_surname, DNI, street, PC, city, municipality, mail, birth_date, allergies, remarks) 
+VALUES (1, 'Casiano', 'Chapas', '98765432B', 'Tontorron', '54321', 'Sama', 'Langreo', 'casiano@gijon11x12.com', '1989-05-15', 'Aspirina', 'Sin observaciones');
