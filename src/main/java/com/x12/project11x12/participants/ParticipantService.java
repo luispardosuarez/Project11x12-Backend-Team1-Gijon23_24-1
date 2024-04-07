a4526c6c6476a557a0292b8702afe818cbf007b7
Luis Pardo <luispardosuarez@gmail.com>
2024-04-07 13:01:16 +0200

    Refactored ParticipantService to remove exception handling and updated comments to reflect no deletion operation

--- src/main/java/com/x12/project11x12/participants/ParticipantService.java
+++ src/main/java/com/x12/project11x12/participants/ParticipantService.java
@@ -1,5 +1,48 @@
-package com.x12.project11x12.participant;
+package com.x12.project11x12.participants;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class ParticipantService {
-  
+
+    private final ParticipantRepository participantRepository;
+
+    @Autowired
+    public ParticipantService(ParticipantRepository participantRepository) {
+        this.participantRepository = participantRepository;
+    }
+
+    public List<Participant> getAllParticipants() {
+        return participantRepository.findAll();
+    }
+
+    public Participant getParticipantById(Long id) {
+        return participantRepository.findById(id).orElse(null);
+    }
+
+    public Participant createParticipant(Participant participant) {
+        return participantRepository.save(participant);
+    }
+
+    public Participant updateParticipant(Long id, Participant participantDetails) {
+        Participant participant = participantRepository.findById(id).orElse(null);
+        if (participant == null) {
+            return null;
+        }
+        participant.setParticipantName(participantDetails.getParticipantName());
+        participant.setParticipantSurname(participantDetails.getParticipantSurname());
+        participant.setDni(participantDetails.getDni());
+        participant.setStreet(participantDetails.getStreet());
+        participant.setPc(participantDetails.getPc());
+        participant.setCity(participantDetails.getCity());
+        participant.setMunicipality(participantDetails.getMunicipality());
+        participant.setMail(participantDetails.getMail());
+        participant.setBirthDate(participantDetails.getBirthDate());
+        participant.setAllergies(participantDetails.getAllergies());
+        participant.setRemarks(participantDetails.getRemarks());
+        return participantRepository.save(participant);
+    }
 }
