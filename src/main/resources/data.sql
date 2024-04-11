864d1018a3b0d75b1bb60293db9fa8301bcb044f
Alicia Gonzalez Castro <aliglezcastro@gmail.com>
2024-04-11 12:26:07 +0200

    añadidos más registros a la tabla scholarships

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,3 +1,12 @@
 -- Scholarship
 
 INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '10000000X');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '20000000Y');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '30000000Z');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '40000000J');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '50000000K');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '60000000V');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '70000000H');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '80000000T');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '90000000R');
+INSERT INTO scholarships (id_scholarship, dni) VALUES (default, '11000000W');
